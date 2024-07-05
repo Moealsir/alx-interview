1ead84728e7734ff626b5543e421749d4540a173
Moealsir <mohamedwdalsir@gmail.com>
2024-07-05 05:27:38 +0300

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Module for handling lockboxes.
+'''
+
+def canUnlockAll(boxes):
+    '''Determines if all boxes in a list of boxes containing keys
+    (indices) to other boxes can be unlocked, starting with the first
+    box being unlocked.
+    '''
+    n = len(boxes)
+    unlocked = set([0])
+    keys = set(boxes[0]).difference(unlocked)
+    while keys:
+        key = keys.pop()
+        if key < 0 or key >= n:
+            continue
+        if key not in unlocked:
+            keys.update(boxes[key])
+            unlocked.add(key)
+    return len(unlocked) == n
